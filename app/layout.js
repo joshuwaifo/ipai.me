7db59fbf97af6322eb52e744ff50acc26bda0c30
Joshua Uwaifo <o.j.uwaifo@gmail.com>
2024-08-22 10:01:36 +0100

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -4,7 +4,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "IP AI",
+  title: "AI IP for you",
   description: "Created by Joshua Uwaifo",
 };
 
